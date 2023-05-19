af406f617b77a46355c3e0ce327c6e280307dfe7
karnishitrit <karniishitrit@gmail.com>
2023-05-19 17:25:08 +0300

    creating the fruit item component just with styling- without any functionality

---
+++ src/components/FruitItem.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import Slider from "@mui/material/Slider";
+
+function FruitItem({ item }) {
+  const trackColor = "#c6c0c0"; // Define the desired track color
+  const trackFillColor = "#b6b2b2"; // Define the desired fiiled track color
+  const thumbColor = "#fff"; // Define the desired thumb color
+
+  const handleDeleteItem = () => {
+    //
+  };
+
+  return (
+    <div className="fruit-item">
+      <div className="fruit-item__header">
+        <div
+          className="fruit-item__header--title"
+          style={{ backgroundColor: item.color }}
+        >
+          <span>{item.icon}</span>
+          {item.name}
+        </div>
+        <div className="fruit-item__header--close" onClick={handleDeleteItem}>
+          <AiOutlineCloseCircle />
+        </div>
+      </div>
+      <div className="fruit-item__quantity">
+        select quantity:
+        <span className="fruit-item__quantity--amount">
+          <span className="fruit-item__quantity--amount--number">0</span>
+          KG
+        </span>
+      </div>
+      <Slider
+        className="fruit-item__slider"
+        aria-label="Temperature"
+        defaultValue={0}
+        valueLabelDisplay="auto"
+        max={item.stock}
+        sx={{
+          "& .MuiSlider-track": {
+            backgroundColor: trackFillColor,
+            border: `2px solid ${thumbColor}`,
+          },
+          "& .MuiSlider-rail": {
+            backgroundColor: trackColor,
+          },
+          "& .MuiSlider-thumb": {
+            backgroundColor: thumbColor,
+            boxShadow: "None",
+          },
+        }}
+      />
+      <div className="fruit-item__price">
+        Price:
+        <span className="fruit-item__price--number">${item.price}</span>
+      </div>
+    </div>
+  );
+}
+
+export default FruitItem;
--- src/pages/FruitPage.jsx
+++ src/pages/FruitPage.jsx
@@ -1,20 +1,21 @@
-// import { useFetchFruitsQuery } from "../store";
+import { useFetchFruitsQuery } from "../store";
 import DropDown from "../components/DropDown";
+import FruitItem from "../components/FruitItem";
 
 function FruitPage() {
-  // const { data, isError, isLoading } = useFetchFruitsQuery("apple");
+  const { data, isError, isLoading } = useFetchFruitsQuery("apple");
 
-  // if (isError) {
-  //   return <div> {isError}</div>;
-  // }
+  if (isError) {
+    return <div> {isError}</div>;
+  }
 
-  // if (isLoading) {
-  //   return (
-  //     <div className="menu">
-  //       <div> {isError}</div>;
-  //     </div>
-  //   );
-  // }
+  if (isLoading) {
+    return (
+      <div className="menu">
+        <div> {isError}</div>;
+      </div>
+    );
+  }
 
   const fruits = [
     "Apple",
@@ -26,6 +27,7 @@ function FruitPage() {
     "Salad",
   ];
 
+  console.log(data);
   return (
     <div className="fruit-page">
       <DropDown
@@ -33,6 +35,7 @@ function FruitPage() {
         title="Select to add item to basket"
         className="fruit-page__drop-down"
       />
+      <FruitItem item={data} />
     </div>
   );
 }
